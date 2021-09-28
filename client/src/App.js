78a5a103a16cd7f43d811372cd2753519305f658
joshbm-git <32262804+joshbm-git@users.noreply.github.com>
2021-09-28 13:09:06 +0200

    Added js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,64 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
+import { Router } from "@reach/router";
+
+import Recipes from "./Recipes";
+import Recipe from "./Recipe";
+
 const API_URL = process.env.REACT_APP_API;
 
 function App() {
-  const [data, setData] = useState("No data :(");
+  const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
-    async function getData() {
-      const url = `${API_URL}/hello`;
+  useEffect(() => { 
+    const fetchData = async () => {
+      //const url = "http://localhost:8081/api/cooking";
+      const url = `${API_URL}/cooking`;
+
       const response = await fetch(url);
       const data = await response.json();
-      setData(data.msg);
-    }
-    getData();
-  }, []);
+      setRecipes(data);
+    }; 
+    fetchData();
+  }, []); 
+  
+  function getRecipe(id) {
+    return recipes.find(recipe => recipe.id === parseInt(id));
+  }
+
+  function addRecipe(title, desc, ingredients) {
+    console.log(title, desc, ingredients);
+
+    const data = { 
+      title: title, 
+      description: desc,
+      ingredients: ingredients    
+    };
+    const postData = async () => {
+      //const url = "http://localhost:8081/api/cooking";
+      const url = `${API_URL}/cooking`;
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const reply = await response.json();
+      console.log(reply);
+
+    }; 
+    postData();
+  }
 
   return (
     <>
-      <h1>MERN App!</h1>
-      <p>Data from server: {data}</p>
+      <h1>Cooking Recipes</h1>
+
+      <Router>
+        <Recipes path="/" data={recipes} addRecipe={addRecipe} ></Recipes>
+        <Recipe path="/recipe/:id" getRecipe={getRecipe} ></Recipe>
+      </Router>
     </>
   );
 }
